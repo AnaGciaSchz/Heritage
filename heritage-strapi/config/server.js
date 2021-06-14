513674304ec34578ea9f94729148fc4de36c1147
Ana <anags.clm@gmail.com>
2021-06-14 19:16:57 +0200

    Adding Strapi

---
+++ heritage-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3a2c20e1babfd7615041bea28de8ed1b'),
+    },
+  },
+});
---
+++ heritage-strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5dc2746e-80e1-4d68-bbae-ac636b034371'
+};

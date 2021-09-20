807f16a9664bea54393d970424fc6245262978c3
AnaGciaSchz <anags.clm@gmail.com>
2021-09-20 15:38:10 +0200

    Adding card transition and working with the grid

--- heritage/components/SearchCard/SearchCard.js
+++ heritage/components/SearchCard/SearchCard.js
@@ -4,21 +4,40 @@ import React from 'react'
 
 function SearchCard(props){
   const [isRotated, setIsRotated] = React.useState(false);
-  const onRotate = () => setIsRotated((rotated) => !rotated);
+  const [isNotBeenRotated, setIsNotBeenRotated] = React.useState(true);
+  const onRotate = (boolean) => {setIsNotBeenRotated(false), setIsRotated(boolean);}
     return(
-        <div className={styles.card} onClick={onRotate}>
-          <p className={styles.name}>{props.name}</p>
-          <Image className={styles.image}
-            src={props.img}
-            alt={props.alt}
-            layout= 'responsive'
-            width = "0"
-            height="0"
-            objectFit="contain"
-          />
-      <p className={styles.firtsLine}>{props.firtsLine}{isRotated ? 'rotated' : 'not rotated'}`</p>
-      <p className={styles.text}>{props.text}</p>
-      </div>
+      <div>
+        {isRotated? null:
+      <div
+      className={isNotBeenRotated? styles.card : isRotated ? styles.quitCard : styles.showCard} 
+        onClick={() => onRotate(!isRotated)}>
+    <p className={styles.name}>{props.name}</p>
+    <p className={styles.date}>Registro: {props.date}</p>
+    <Image className={styles.image}
+          src={props.img}
+          alt={props.alt}
+          layout= 'responsive'
+          width = "0"
+          height="0"
+          objectFit="contain"
+        />
+    <p className={styles.firtsLine}>{props.firtsLine}</p>
+    <p className={styles.text}>{props.text}</p>
+    </div>}
+    {!isRotated? null:
+    <div
+      className={isRotated ? styles.showCard : styles.quitCard} 
+        onClick={() => onRotate(!isRotated)}>
+    <p className={styles.name}>{props.name}</p>
+    <p className={styles.date}>Registro: {props.date}</p>
+    <p className={styles.firtsLine}>Descripción</p>
+    <p className={styles.text}>{props.description}</p>
+    <p className={styles.firtsLine}>Ha estado en</p>
+    <p className={styles.text}>{props.been}</p>
+    <p className={styles.social}>Redes: Red Red Linkedin</p>
+    </div>}
+    </div>
     )
     
 }
--- heritage/pages/BusquedaGenerica.js
+++ heritage/pages/BusquedaGenerica.js
@@ -4,12 +4,68 @@ function BusquedaGenerica(){
         <div>
             <p><b>Español</b>/Asturianu/English</p>
       <h1 className = "title1">Página de Fichas genérica</h1>
+      <div className="searchFeed">
       <SearchCard
         name = "Ana María García Sánchez"
         img = "/Ana.jpg"
         alt = "Imagen"
         firtsLine = "Promoción 2020-2021"
-        text = "Estudiante de cuarto curso, está intentando recordar cómo se programaba con React."/>
+        text = "Estudiante de cuarto curso, está intentando recordar cómo se programaba con React."
+        date = "20/09/2021"
+        description = "Carta de prueba para ver cómo quedarían las cartas. Este texto tiene 120 caracteres, que sería lo mínimo que se pediría."
+        been =  "Carta de prueba para ver cómo quedarían las cartas. Este texto tiene 120 caracteres, que sería lo mínimo que se pediría."
+        />
+        <SearchCard
+        name = "Ana María García Sánchez"
+        img = "/Ana.jpg"
+        alt = "Imagen"
+        firtsLine = "Promoción 2020-2021"
+        text = "Estudiante de cuarto curso, está intentando recordar cómo se programaba con React."
+        date = "20/09/2021"
+        description = "Carta de prueba para ver cómo quedarían las cartas. Este texto tiene 120 caracteres, que sería lo mínimo que se pediría."
+        been =  "Carta de prueba para ver cómo quedarían las cartas. Este texto tiene 120 caracteres, que sería lo mínimo que se pediría."
+        />
+        <SearchCard
+        name = "Ana María García Sánchez"
+        img = "/Ana.jpg"
+        alt = "Imagen"
+        firtsLine = "Promoción 2020-2021"
+        text = "Estudiante de cuarto curso, está intentando recordar cómo se programaba con React."
+        date = "20/09/2021"
+        description = "Carta de prueba para ver cómo quedarían las cartas. Este texto tiene 120 caracteres, que sería lo mínimo que se pediría."
+        been =  "Carta de prueba para ver cómo quedarían las cartas. Este texto tiene 120 caracteres, que sería lo mínimo que se pediría."
+        />
+        <SearchCard
+        name = "Ana María García Sánchez"
+        img = "/Ana.jpg"
+        alt = "Imagen"
+        firtsLine = "Promoción 2020-2021"
+        text = "Estudiante de cuarto curso, está intentando recordar cómo se programaba con React."
+        date = "20/09/2021"
+        description = "Carta de prueba para ver cómo quedarían las cartas. Este texto tiene 120 caracteres, que sería lo mínimo que se pediría."
+        been =  "Carta de prueba para ver cómo quedarían las cartas. Este texto tiene 120 caracteres, que sería lo mínimo que se pediría."
+        />
+        <SearchCard
+        name = "Ana María García Sánchez"
+        img = "/Ana.jpg"
+        alt = "Imagen"
+        firtsLine = "Promoción 2020-2021"
+        text = "Estudiante de cuarto curso, está intentando recordar cómo se programaba con React."
+        date = "20/09/2021"
+        description = "Carta de prueba para ver cómo quedarían las cartas. Este texto tiene 120 caracteres, que sería lo mínimo que se pediría."
+        been =  "Carta de prueba para ver cómo quedarían las cartas. Este texto tiene 120 caracteres, que sería lo mínimo que se pediría."
+        />
+         <SearchCard
+        name = "Ana María García Sánchez"
+        img = "/Ana.jpg"
+        alt = "Imagen"
+        firtsLine = "Promoción 2020-2021"
+        text = "Estudiante de cuarto curso, está intentando recordar cómo se programaba con React."
+        date = "20/09/2021"
+        description = "Carta de prueba para ver cómo quedarían las cartas. Este texto tiene 120 caracteres, que sería lo mínimo que se pediría."
+        been =  "Carta de prueba para ver cómo quedarían las cartas. Este texto tiene 120 caracteres, que sería lo mínimo que se pediría."
+        />
+        </div>
         </div>
     );
 

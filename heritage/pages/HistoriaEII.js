1d334e62a58d25a6cf598ca5c08c6feb90c18ce5
AnaGciaSchz <anags.clm@gmail.com>
2021-10-05 19:13:52 +0200

    Adding an editor

--- heritage/pages/HistoriaEII.js
+++ heritage/pages/HistoriaEII.js
@@ -1,5 +1,28 @@
+import React, {useState} from 'react';
+import dynamic from "next/dynamic";
+import 'suneditor/dist/css/suneditor.min.css'; // Import Sun Editor's CSS File
+
+const SunEditor = dynamic(() => import("suneditor-react"), {
+    ssr: false,
+  });
+  const {buttonList} = dynamic(() => import("suneditor-react"), {
+    ssr: false,
+  });
+
 function HistoriaEII() {
-    return (<p>Insertar historia aquí</p>);
+    const [content, setContent] = useState('');
+    const handleChange = (content) => setContent(content);
+    return ( 
+    <div> 
+    <div dangerouslySetInnerHTML={{__html: content}} />
+    <SunEditor defaultValue={content}  onChange={handleChange} lang="es"
+    setOptions={{
+        buttonList: buttonList // Or Array of button list, eg. [['font', 'align'], ['image']]
+        // plugins: [font] set plugins, all plugins are set by default
+        // Other option
+}}/>
+    </div>
+    )
 
 }
 

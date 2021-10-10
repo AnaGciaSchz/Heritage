586452ad9a1d7dfe4a9ebe578dafcfb923f41b94
AnaGciaSchz <anags.clm@gmail.com>
2021-10-10 19:34:53 +0200

    Save button created for editor

--- heritage/pages/HistoriaEII.js
+++ heritage/pages/HistoriaEII.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import dynamic from "next/dynamic";
 import 'suneditor/dist/css/suneditor.min.css'; // Import Sun Editor's CSS File
 
@@ -10,17 +10,23 @@ const SunEditor = dynamic(() => import("suneditor-react"), {
   });
 
 function HistoriaEII() {
+  const editor = useRef();
+  const getSunEditorInstance = (sunEditor) => {
+    editor.current = sunEditor;
+};
     const [content, setContent] = useState('');
-    const handleChange = (content) => setContent(content);
+
+    const handleClick = () => setContent(editor.current.getContents());
     return ( 
     <div> 
     <div dangerouslySetInnerHTML={{__html: content}} />
-    <SunEditor defaultValue={content}  onChange={handleChange} lang="es"
+    <SunEditor getSunEditorInstance={getSunEditorInstance} defaultValue={content}  lang="es"
     setOptions={{
         buttonList: buttonList // Or Array of button list, eg. [['font', 'align'], ['image']]
         // plugins: [font] set plugins, all plugins are set by default
         // Other option
 }}/>
+<button onClick={handleClick}>Guardar</button>
     </div>
     )
 

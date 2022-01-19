25932d7d56a1cf16ea922070eef00c588d1f7168
AnaGciaSchz <anags.clm@gmail.com>
2022-01-19 19:52:44 +0100

    changing all search pages

--- heritage/pages/ZonaDelegacion.js
+++ heritage/pages/ZonaDelegacion.js
@@ -3,22 +3,27 @@ import { useState } from "react";
 import { useIntl } from "react-intl"
 import SearchBar from "components/SearchComponents/SearchBar/SearchBar.js"
 
+import {useDispatch,} from 'react-redux';
+import {changeInSearchBarByUser} from '../services/redux/features/search/searchSlice.js';
+
 export default function ZonaDelegacion() {
+
     const {formatMessage} = useIntl();
     const f = id => formatMessage({ id })
+
     const [query, setQuery] = useState("");
-    const [change, setChange] = useState(false);
+
+    const dispatch = useDispatch() 
     return (
         <>
             <h1 className="title1">{f("ZDelegacion")}</h1>
             <SearchBar
-            setQuery = {setQuery}
-            setChange={setChange}
+            setQuery={setQuery}
+            setChange={() => dispatch(changeInSearchBarByUser())}
             />    
             <SearchLayout
                 query={query}
                 index="delegate-card"
-                change={change}
             />                      
         </>
     );

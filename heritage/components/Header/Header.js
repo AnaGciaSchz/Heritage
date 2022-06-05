ab5f98364f003ef89f286316ba3adf457963c146
AnaGciaSchz <anags.clm@gmail.com>
2022-06-05 19:47:41 +0200

    Logged in header fixed

--- heritage/components/Header/Header.js
+++ heritage/components/Header/Header.js
@@ -17,25 +17,33 @@ export default function Header() {
 
     useEffect(() => {
         if (cookieCutter.get('userName') != null) {
-            setHeader(<header className={styles.tuputamadre}>
-                               
-                <nav className={styles.tuweamadre}>
-                <MenuButton nombre={f("historiaEII")} src="headerIcons/historiaEII.png" referencia="/HistoriaEII" />
-                <MenuButton nombre={f("ZExalumnado")} src="headerIcons/egresados.png" referencia="/ZonaEgresados" />
-                <MenuButton nombre={f("ZProfesorado")} src="headerIcons/profesorado.png" referencia="/ZonaProfesorado" />
-                <MenuButton nombre={f("ZDelegacion")} src="headerIcons/delegados.png" referencia="/ZonaDelegacion"/>
+            setHeader(<header className={styles.container}>
 
-                <div className={styles.logo}>
+                <div className={styles.logoArriba}>
                     <a href={"/" + router.locale}>
-                       
-                        <img src='/HeritageLogo.png' alt='logo'/>
+
+                        <img src='/HeritageLogo.png' alt='logo' />
                     </a>
                 </div>
 
-                <MenuButton nombre={f("EditorHistoria")} src="headerIcons/admin.png" referencia="/EditorHistoria"/>
-                <MenuButton nombre={f("SubidaDeCartas")} src="headerIcons/subidaCartas.png" referencia="/SubidaDeCartas" />
-                <MenuButton nombre={f("QueEsHeritage")} src="headerIcons/queEsHeritage.png" referencia="/about" />
-                <MenuButton nombre={f("Contacto")} src="headerIcons/contacto.png" referencia="/Contacto" />
+
+                <nav className={styles.menu}>
+                    <MenuButton nombre={f("historiaEII")} src="headerIcons/historiaEII.png" referencia="/HistoriaEII" />
+                    <MenuButton nombre={f("ZExalumnado")} src="headerIcons/egresados.png" referencia="/ZonaEgresados" />
+                    <MenuButton nombre={f("ZProfesorado")} src="headerIcons/profesorado.png" referencia="/ZonaProfesorado" />
+                    <MenuButton nombre={f("ZDelegacion")} src="headerIcons/delegados.png" referencia="/ZonaDelegacion" />
+
+                    <div className={styles.logoAbajo}>
+                        <a href={"/" + router.locale}>
+
+                            <img src='/HeritageLogo.png' alt='logo'  className={styles.logo}/>
+                        </a>
+                    </div>
+
+                    <MenuButton nombre={f("EditorHistoria")} src="headerIcons/admin.png" referencia="/EditorHistoria"/>
+                    <MenuButton nombre={f("SubidaDeCartas")} src="headerIcons/subidaCartas.png" referencia="/SubidaDeCartas" />
+                    <MenuButton nombre={f("QueEsHeritage")} src="headerIcons/queEsHeritage.png" referencia="/about" />
+                    <MenuButton nombre={f("Contacto")} src="headerIcons/contacto.png" referencia="/Contacto" />
                 </nav>
 
 
@@ -48,7 +56,7 @@ export default function Header() {
                 <MenuButton nombre={f("ZDelegacion")} src="headerIcons/delegados.png" referencia="/ZonaDelegacion" />
 
                 <span className={styles.logo}>
-                <a href={"/" + router.locale} target="_blank">
+                    <a href={"/" + router.locale} target="_blank">
                         <Image className={styles.image}
                             src="/HeritageLogo.png"
                             alt={f("logo")}
